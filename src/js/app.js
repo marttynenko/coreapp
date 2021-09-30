9a5c570562a99b5bb06c4e4fe36b519f0cb10d81
marttynenko <marttynenko@gmail.com>
2021-09-30 14:13:21 +0300

    bugs

--- src/js/app.js
+++ src/js/app.js
@@ -16,9 +16,9 @@ let app = {
     $('.nav__link__drop').on('mouseover', function () {
       $('.menu').addClass('opened');
     });
-    // $(document).on('scroll', function () {
-    //   $('.menu').fadeOut();
-    // })
+    $('.menu').on('mouseleave', function () {
+      $('.menu').removeClass('opened');
+    });
     function checkTargets(target, origin) {
       if ($(target).hasClass(origin) || $(target).closest('.'+origin).length) {
         return false
@@ -29,18 +29,26 @@ let app = {
       var menu = $(".menu");
       if (!menu.is(e.target) && menu.has(e.target).length === 0 && checkTargets(e.target,'header__menu-toggler')) {
         menu.removeClass('opened');
+        $('.header__menu-toggler').removeClass('opened')
       }
     });
 
     $('.--openpopup').on('click', function () {
       const data = $(this).data('popup');
       $('.popup[data-popup="' + data + '"]').fadeIn();
-      $('.body').addClass('--frozen');
+      $('.popup-bg[data-popup="' + data + '"]').fadeIn();
+      $('html').addClass('--overflowhidden');
     })
 
-    $('.popup__bg').on('click', function () {
+    $(document).on('click','.popup__bg', function () {
       $('.popup').fadeOut();
-      $('.body').removeClass('--frozen');
+      $('html').removeClass('--overflowhidden');
+    })
+
+    $(document).on('click','.popup-close', function (e) {
+      e.preventDefault();
+      $('.popup, .popup-bg').fadeOut();
+      $('html').removeClass('--overflowhidden');
     })
 
 
